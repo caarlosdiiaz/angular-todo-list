008290d50d6fb937c604051a8feed5bed83cc638
caarlosdiiaz <diazromerocarlos2@gmail.com>
2025-05-14 12:18:58 +0100

    proyecto creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoAppComponent } from "./components/todo-app.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [TodoAppComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'todo-list';
---
+++ src/app/components/todo-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-app.component.html'
+})
+export class TodoAppComponent {
+
+}
